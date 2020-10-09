444148f5ff709170538e10887d1d45f9eccd975a
rodrigojpt <rodrigojpereirat@gmail.com>
2020-10-09 15:12:03 -0400

    fixed api call and response handling

--- app.js
+++ app.js
@@ -1,21 +1,26 @@
 const mainContainer = document.getElementById('main-container');
 const startButton = document.querySelector('.start-game');
+const gameState = {};
 
 startButton.addEventListener('click', startGame);
 
 function startGame(e) {
 	e.preventDefault();
 	mainContainer.innerHTML = '';
-	getSessionQuestions().then((res) => {
-		console.log('should be second');
+	getSessionQuestions().then((questions) => {
+		gameState.questions = questions;
 	});
 }
 
 async function getSessionQuestions() {
+	const questions = [];
 	await fetch('https://opentdb.com/api.php?amount=10&category=14')
 		.then((res) => res.json())
 		.then((resJson) => {
-			console.log(resJson, 'should be first');
-			return resJson;
-		});
+			resJson.results.forEach((question) => {
+				questions.push(question);
+			});
+		})
+		.catch((error) => alert(error));
+	return questions;
 }
