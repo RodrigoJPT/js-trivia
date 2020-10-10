be1d6acd33cd0b7da610b538668e5daa9aab0811
rodrigojpt <rodrigojpereirat@gmail.com>
2020-10-10 17:41:43 -0400

    button update pipeline started

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const mainContainer = document.getElementById('main-container');
 const mainMenu = document.getElementById('main-menu');
 const startButton = document.querySelector('.start-game');
+startButton.addEventListener('click', startGame);
 const gameState = {};
 const endScreen = document.createElement('div');
 const endScreenText = document.createElement('p');
@@ -13,12 +14,17 @@ replayButton.innerText = 'Play Again';
 replayButton.addEventListener('click', startGame);
 const questionScreen = document.createElement('div');
 const questionText = document.createElement('p');
+const buttonContainer = document.createElement('div');
+//buttonContainer.addEventListener('click', checkAnswer);
+for (i = 0; i < 4; i++) {
+	let newButton = document.createElement('button');
+	newButton.classList.add('question-button');
+	questionScreen.appendChild(newButton);
+}
 endScreen.appendChild(endScreenText);
 endScreen.appendChild(menuButton);
 endScreen.appendChild(replayButton);
 questionScreen.appendChild(questionText);
-
-startButton.addEventListener('click', startGame);
 
 async function getSessionQuestions() {
 	const questions = [];
@@ -47,12 +53,32 @@ function startGame(e) {
 function update(index) {
 	if (index < gameState.questions.length) {
 		questionText.innerHTML = gameState.questions[index].question;
+		getButtonAnswers(gameState.questions[index]);
 		mainContainer.innerHTML = '';
 		mainContainer.appendChild(questionScreen);
 		gameState.currentQuestion++;
 	} else {
 		endGame();
 	}
+}
+
+function getButtonAnswers(question) {
+	const answers = [];
+	answers.push({
+		text: question.correct_answer,
+		correct: true,
+	});
+	question.incorrect_answers.forEach((answer) => {
+		answers.push({
+			text: answer,
+			correct: false,
+		});
+	});
+	setAnswerButtonText(answers);
+}
+
+function setAnswerButtonText(answers) {
+	console.log(answers);
 }
 
 function endGame(correct = 6, score = null) {
